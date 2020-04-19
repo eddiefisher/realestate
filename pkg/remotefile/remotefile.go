b2971450a9f6baa7f243c78a7ee892ad455bc891
Eddie Fisher <2085415+eddiefisher@users.noreply.github.com>
2020-04-20 02:49:45 +0300

    #0 do not download file to local storage

--- pkg/remotefile/remotefile.go
+++ pkg/remotefile/remotefile.go
@@ -1,10 +1,7 @@
 package remotefile
 
 import (
-	"io"
-	"net/http"
 	"net/url"
-	"os"
 	"path/filepath"
 	"strings"
 
@@ -36,17 +33,6 @@ func (r *RemoteFile) Download() error {
 		return err
 	}
 
-	// Create blank file
-	file, err := r.createFile()
-	if err != nil {
-		return err
-	}
-
-	// Put content on file
-	err = r.putFile(file, r.httpClient())
-	if err != nil {
-		return err
-	}
 	r.cloudinaryUpload()
 	return nil
 }
@@ -56,21 +42,6 @@ func (r *RemoteFile) cloudinaryUpload() {
 	var extension = filepath.Ext(r.Name)
 	var name = r.Name[0 : len(r.Name)-len(extension)]
 	imageproxy.Upload(r.URL, r.Prefix, name)
-}
-
-func (r *RemoteFile) putFile(file *os.File, client *http.Client) error {
-	resp, err := client.Get(r.URL)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	_, err = io.Copy(file, resp.Body)
-	defer file.Close()
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func (r *RemoteFile) buildFileName() error {
@@ -83,27 +54,4 @@ func (r *RemoteFile) buildFileName() error {
 	r.Name = segments[len(segments)-1]
 
 	return nil
-}
-
-func (r *RemoteFile) httpClient() *http.Client {
-	client := http.Client{
-		CheckRedirect: func(r *http.Request, via []*http.Request) error {
-			r.URL.Opaque = r.URL.Path
-			return nil
-		},
-	}
-
-	return &client
-}
-
-func (r *RemoteFile) createFile() (*os.File, error) {
-	path := r.Path + "/" + r.Prefix
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		os.MkdirAll(path, os.ModePerm)
-	}
-	file, err := os.Create(path + "/" + r.Name)
-	if err != nil {
-		return nil, err
-	}
-	return file, nil
 }
